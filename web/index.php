ec9f917f2eb6bf37dffff3f522c8e7d480f55c25
Kevin Phair <kevinphair@Kevins-MacBook-Pro.local>
2019-01-18 23:03:02 -0700

    just work

--- web/index.php
+++ web/index.php
@@ -40,6 +40,7 @@
     </div>
   </div>
   <script src="js/grid.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script>
     $(function(){
       Grid.init();
