461f0de7704dd25ca6abccbf6c7ecd65a5dab378
Kevin Phair <kevinphair@Kevins-MacBook-Pro.local>
2019-01-18 22:27:18 -0700

    added some content

--- web/index.php
+++ web/index.php
@@ -6,12 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <!-- <script src="a.js"></script> -->
-  <link href="style.css?v=1.1.1" rel="stylesheet" type="text/css">
+  <link href="style.css?v=1.1.2" rel="stylesheet" type="text/css">
   <title>Kevin Phair - CS313</title>
 </head>
 <body>
   <div class="main-area">
     <h1 class="indent title-text">Kevin Phair - CS313</title>
+    <h2 class="indent">Professional Student-level Web Design</h2>
   </div>
 </body>
 </html>
