137178710bf1ff99169337485f799ea407891088
Kevin Phair <kevinphair@Kevins-MacBook-Pro.local>
2019-01-18 22:40:41 -0700

    adding some sweet js files

--- web/index.php
+++ web/index.php
@@ -6,13 +6,20 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <!-- <script src="a.js"></script> -->
-  <link href="style.css?v=1.1.5" rel="stylesheet" type="text/css">
+  <script src="js/grid.js"></script>
+  <script src="js/modernizr.custom.js"></script>
+  <link href="css/style.css?v=1.1.5" rel="stylesheet" type="text/css">
+  <link href="css/components.css?v=1" rel="stylesheet" type="text/css">
   <title>Kevin Phair - CS313</title>
 </head>
 <body>
   <div class="main-area">
     <h1 class="indent title-text">Kevin Phair - CS313</title>
     <h3 class="indent subtitle-text">Professional Student-Level Web Design</h3>
+  </div>
+
+  <div class="content-area">
+    
   </div>
 </body>
 </html>
