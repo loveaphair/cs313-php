102f355526ec5e51b6ba607f1777d209e6b9f510
Kevin Phair <kevinphair@Kevins-MacBook-Pro.local>
2019-01-18 23:20:25 -0700

    just work

--- web/index.php
+++ web/index.php
@@ -5,11 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <!-- <script src="a.js"></script> -->
-  <script src="js/modernizr.custom.js"></script>
   <link href="css/style.css?v=1.1.5" rel="stylesheet" type="text/css">
-  <link href="css/component.css?v=1" rel="stylesheet" type="text/css">
   <title>Kevin Phair - CS313</title>
 </head>
 <body>
@@ -17,34 +14,7 @@
     <h1 class="indent title-text">Kevin Phair - CS313</title>
     <h3 class="indent subtitle-text">Professional Student-Level Web Design</h3>
     <div class="content-area">
-      <ul id="og-grid" class="og-grid">
-        <li>
-          <a href="#" data-largsrc="images/week2.png" data-title="Week 2 Team Assignment" data-description="Team Assignment completed in Week 2">
-            <img src="images/week2_tn.png" alt="Week2">
-          </a>
-          <div class="og-expander">
-            <div class="og-expander-inner">
-              <span class="og-close"></span>
-              <div class="og-fullimg">
-                <div class="og-loading"></div>
-                <img src="images/week2.png">
-              </div>
-              <div class="og-details">
-                <h3>Veggies sunt bona vobis</h3>
-                <p>Komatsuna prairie turnip wattle seed artichoke mustard horseradish taro rutabaga ricebean carrot black-eyed pea turnip greens beetroot yarrow watercress kombu.</p>
-                <a href="http://cargocollective.com/jaimemartinez/">Visit website</a>
-              </div>
-            </div>
-          </div>
-        </li>
-      </ul>
     </div>
   </div>
-  <script src="js/grid.js"></script>
-  <script>
-    $(function(){
-      Grid.init();
-    });
-  </script>
 </body>
 </html>
