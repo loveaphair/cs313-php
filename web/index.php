11933199a339289007843e8e78b92147e2fb2ba1
Kevin Phair <kevinphair@Kevins-MacBook-Pro.local>
2019-01-18 22:28:34 -0700

    update some style settings

--- web/index.php
+++ web/index.php
@@ -6,13 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <!-- <script src="a.js"></script> -->
-  <link href="style.css?v=1.1.2" rel="stylesheet" type="text/css">
+  <link href="style.css?v=1.1.3" rel="stylesheet" type="text/css">
   <title>Kevin Phair - CS313</title>
 </head>
 <body>
   <div class="main-area">
     <h1 class="indent title-text">Kevin Phair - CS313</title>
-    <h2 class="indent">Professional Student-level Web Design</h2>
+    <h2 class="indent" style="margin-top:-30px;">Professional Student-level Web Design</h2>
   </div>
 </body>
 </html>
