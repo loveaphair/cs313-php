c58e276abfc458abdc5dc2d0ed4ee568e53a3c44
Kevin Phair <kevinphair@Kevins-MacBook-Pro.local>
2019-01-18 22:18:05 -0700

    modified the index pagae to be something special

--- web/index.php
+++ web/index.php
@@ -1,5 +1,17 @@
-<?php
-
-  phpinfo();
-
-?>
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- <script src="a.js"></script> -->
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <title>Kevin Phair - CS313</title>
+</head>
+<body>
+  <div class="main-area">
+    <h1 class="indent">Kevin Phair - CS313</title>
+  </div>
+</body>
+</html>
