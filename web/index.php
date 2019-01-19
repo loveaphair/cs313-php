17efa36d1e6bfa53750ec5e92e59437f3b3aeb28
Kevin Phair <kevinphair@Kevins-MacBook-Pro.local>
2019-01-18 22:23:23 -0700

    updated styling

--- web/index.php
+++ web/index.php
@@ -1,4 +1,4 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -6,12 +6,12 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <!-- <script src="a.js"></script> -->
-  <link href="style.css?v=1" rel="stylesheet" type="text/css">
+  <link href="style.css?v=1.1" rel="stylesheet" type="text/css">
   <title>Kevin Phair - CS313</title>
 </head>
 <body>
   <div class="main-area">
-    <h1 class="indent">Kevin Phair - CS313</title>
+    <h1 class="indent title-text">Kevin Phair - CS313</title>
   </div>
 </body>
 </html>
