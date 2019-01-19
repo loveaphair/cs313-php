d14487c986867e0ae22c677415673213b673bdb8
Kevin Phair <kevinphair@Kevins-MacBook-Pro.local>
2019-01-18 22:50:47 -0700

    fixin some stuff

--- web/index.php
+++ web/index.php
@@ -9,23 +9,36 @@
   <script src="js/grid.js"></script>
   <script src="js/modernizr.custom.js"></script>
   <link href="css/style.css?v=1.1.5" rel="stylesheet" type="text/css">
-  <link href="css/components.css?v=1" rel="stylesheet" type="text/css">
+  <link href="css/component.css?v=1" rel="stylesheet" type="text/css">
   <title>Kevin Phair - CS313</title>
 </head>
 <body>
   <div class="main-area">
     <h1 class="indent title-text">Kevin Phair - CS313</title>
     <h3 class="indent subtitle-text">Professional Student-Level Web Design</h3>
-  </div>
-
-  <div class="content-area">
-    <ul id="og-grid" class="og-grid">
-      <li>
-        <a href="#" data-largsrc="images/week2.png" data-title="Week 2 Team Assignment" data-description="Team Assignment completed in Week 2">
-          <img src="images/week2_tn.jpg" alt="Week2">
-        </a>
-      </li>
-    </ul>
+    <div class="content-area">
+      <ul id="og-grid" class="og-grid">
+        <li>
+          <a href="#" data-largsrc="images/week2.png" data-title="Week 2 Team Assignment" data-description="Team Assignment completed in Week 2">
+            <img src="images/week2_tn.jpg" alt="Week2">
+          </a>
+          <div class="og-expander">
+            <div class="og-expander-inner">
+              <span class="og-close"></span>
+              <div class="og-fullimg">
+                <div class="og-loading"></div>
+                <img src="images/2.jpg">
+              </div>
+              <div class="og-details">
+                <h3>Veggies sunt bona vobis</h3>
+                <p>Komatsuna prairie turnip wattle seed artichoke mustard horseradish taro rutabaga ricebean carrot black-eyed pea turnip greens beetroot yarrow watercress kombu.</p>
+                <a href="http://cargocollective.com/jaimemartinez/">Visit website</a>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </body>
 </html>
